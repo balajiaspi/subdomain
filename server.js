497dd529244dd5209d2d75edd49115d75da00f8e
Balaji <balajiv@aspigrow.com>
2023-03-05 21:19:30 +0530

    added gitignore

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Route for the home page
+app.get('/', (req, res) => {
+    res.send(`<p>${req.url}</p>`);
+});
+
+app.post('/login', (req, res) => {
+    if( req.body ) {
+        res.redirect(`https://${req.body.username}.vidhyaan.com`);
+    }
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server is listening on port 3000');
+});
